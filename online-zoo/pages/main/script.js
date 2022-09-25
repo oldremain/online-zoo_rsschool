175132743ce5c7c8c74de94ca5e40f5061b24e79
Oleg Zaicev <30oleg.zv94@gmail.com>
2022-09-25 14:50:05 +0300

    feature: add Testimonials section

--- online-zoo/pages/main/script.js
+++ online-zoo/pages/main/script.js
@@ -1,13 +1,27 @@
 window.addEventListener("DOMContentLoaded", () => {
-  const swiper = new Swiper(".swiper", {
-    // Optional parameters
+  const petsSwiper = new Swiper(".pets-swiper", {
     direction: "horizontal",
     loop: true,
-
-    // Navigation arrows
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
+    },
+  });
+
+  const testimonialsSwiper = new Swiper(".testimonials-swiper", {
+    direction: "horizontal",
+    loop: true,
+    // autoplay: {
+    //   delay: 5000,
+    // },
+    speed: 2000,
+    pagination: {
+      el: ".swiper-pagination",
+      horizontalClass: "swiper-pagination-horizontal custom-prop",
+      type: "progressbar",
+    },
+    scrollbar: {
+      el: ".swiper-scrollbar",
     },
   });
 });
